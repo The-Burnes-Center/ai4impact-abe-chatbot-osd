14214186662ddaf11797359999b5cfc61b0b726d
anuttan <anjith16@gmail.com>
2025-12-04 01:05:18 -0500

    Deploy to NonProd

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,10 +3,10 @@ export const AUTHENTICATION = true;
 // change these as needed
 
 // must be unique globally or the deployment will fail
-export const cognitoDomainName = "genaimvp-osd-auth"
+export const cognitoDomainName = "abe-nonprod-osd-auth"
 // this can be anything that would be understood easily, but you must use the same name
 // when setting up a sign-in provider in Cognito
 // make sure to leave it blank if you do not actually have an SSO provider configured in Cognito!
-export const OIDCIntegrationName = "ANF-AI-OSD-Sandbox"
+export const OIDCIntegrationName = "MassGov-Login"
 // this MUST be unique to your account
-export const stackName = "GenAiOsdChatStack"
+export const stackName = "ABEStackNonProd"
